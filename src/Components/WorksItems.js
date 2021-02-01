c86ddfa8af50d6a387d86f19cab81f89a757096d
Obiora Igboanusi <cwertlinks@gmail.com>
2021-02-01 23:36:11 +0100

    implemented services section

---
+++ src/Components/WorksItems.js
@@ -0,0 +1,52 @@
+import web from '../assets/web-dev-icon-2.png';
+
+export const items = [
+  {
+    title: 'Web Developement',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin justo sit amet purus varius, pretium convallis eros varius.',
+    logo: {
+      src: web,
+      altText: 'good text here'
+    }
+  },
+  {
+    title: 'Digital Marketing',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin justo sit amet purus varius, pretium convallis eros varius.',
+    logo: {
+      src: web,
+      altText: 'good text here'
+    }
+  },
+  {
+    title: 'UI/UX design',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin justo sit amet purus varius, pretium convallis eros varius.',
+    logo: {
+      src: web,
+      altText: 'good text here'
+    }
+  },
+  {
+    title: 'App design',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin justo sit amet purus varius, pretium convallis eros varius.',
+    logo: {
+      src: web,
+      altText: 'good text here'
+    }
+  },
+  {
+    title: 'Project Management',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin justo sit amet purus varius, pretium convallis eros varius.',
+    logo: {
+      src: web,
+      altText: 'good text here'
+    }
+  },
+  {
+    title: 'Career Mentorship',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer sollicitudin justo sit amet purus varius, pretium convallis eros varius.',
+    logo: {
+      src: web,
+      altText: 'good text here'
+    }
+  }
+  ]
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 import CallToAction from '../CallToAction'
-import {Hero} from "../Hero";
+import Hero from "../Hero";
+import Works from "../Works";
 
 export default class Home extends Component {
   render() {
@@ -8,6 +9,7 @@ export default class Home extends Component {
       <main>
         <section>
           <Hero />
+          <Works />
           <CallToAction />
         </section>
       </main>

b69a04bbee9e394e36ec55d22730ea89cc296acb
Obiora Igboanusi <cwertlinks@gmail.com>
2021-03-01 14:06:56 +0100

    optimized for accessibility

--- src/Components/WhyWeAreSpecial.js
+++ src/Components/WhyWeAreSpecial.js
@@ -5,33 +5,35 @@ import '../styles/WhyWeAreSpecial.css'
 class WhyWeAreSpecial extends React.Component{
     render(){
         return(
-            <div className='wrapper1'>
-                <div className="content">
-                    <h3>Why We Are Special</h3>
+            <section className='why-special'>
+                <div className='wrapper'>
+                <header className="">
+                    <h2 className='color-primary'>Why We Are Special</h2>
                     <p>
                         Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum at lacus facilisis
                     </p>                    
-                </div>
+                </header>
 
-                <div className="gridContainer">
+                <div className="why-cards">
                     {
                         componentContent.map(function(content, index){
                             return (
-                            <div key={index}>
-                                <h1> {content.title}</h1>
+                            <div key={index} className='why-card'>
+                                <h3> {content.title}</h3>
                                 <div className='innerGrid'>
                                     <div className='svgContainer'>
                                         <span>{content.image}</span>
                                     </div>
-                                    <div className='innerContainer'>
+                                    <div className='why-card-message'>
                                         <p>{content.message}</p>
                                     </div>
                                 </div>
                             </div>
                         )})
                     }
-                </div>    
-            </div>
+                </div>
+                </div>
+            </section>
         )
     }
 }

e4097d0326ecadb96483e152bb9b4b3e97eae29e
Obiora Igboanusi <cwertlinks@gmail.com>
2021-03-01 14:07:29 +0100

    removed the demo text

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -6,7 +6,6 @@ export default class About extends Component {
   render() {
     return (
       <div>
-        <h1>About Works</h1>
         <WhyWeAreSpecial/> 
         <CallToAction/>     
         

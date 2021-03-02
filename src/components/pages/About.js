80760a8f491e83eab57bcacb8ca952a2640a3574
Obiora Igboanusi <cwertlinks@gmail.com>
2021-03-02 18:53:38 +0100

    debugged

--- src/components/pages/About.js
+++ src/components/pages/About.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react'
 import WhyWeAreSpecial from '../WhyWeAreSpecial'
 import CallToAction from '../CallToAction'
-import AboutImage from './AboutImage';
-import AboutWeAre from './AboutWeAre';
+import AboutHero from '../AboutHero';
+import AboutWeAre from '../AboutWeAre';
 import Works  from '../Works';
 
 export default class About extends Component {
   render() {
     return (
       <div>
-        <AboutImage />
+        <AboutHero />
         <AboutWeAre/>
         <Works />
         <WhyWeAreSpecial/> 

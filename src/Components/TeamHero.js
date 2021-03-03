73a7f13e2411b08bcfbbd66bfc448636bca5f873
Obiora Igboanusi <cwertlinks@gmail.com>
2021-03-03 16:33:33 +0100

    created the team hero component

---
+++ src/Components/TeamHero.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function TeamHero() {
+  return (
+    <div className="contact_hero">
+      <div className="wrapper">
+        <div>
+          <h1>Our Bond is Our Secret...</h1>
+          <p>Meet the people behind our magic.</p>
+        </div>
+        {/* <img src={photo} alt="front desk officer" /> */}
+      </div>
+    </div>
+  );
+}

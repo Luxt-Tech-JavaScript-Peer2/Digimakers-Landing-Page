fad06f644c1142db52aeeae633f95b7dc6ca7bce
Obiora Igboanusi <cwertlinks@gmail.com>
2021-03-03 16:33:10 +0100

    added the team data component

---
+++ src/Components/TeamData.js
@@ -0,0 +1,77 @@
+import obiora from "../assets/obiora.jpg";
+
+export const teamData = [
+   {
+    name: "obiora igboanusi",
+    tittle: "front-end developer",
+    role: "Project Manager",
+    image: obiora,
+    socials: {
+      twitter: "https://www.twitter.com/igboanusiobiora",
+      linkedin: "https://www.linkedin.com/c-obioraigboanusi",
+      github: "https://www.github.com/obioraigboanusi",
+    },
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet architecto nulla reprehenderit. Temporibus, odit aliquid? Unde aliquid tempore molestiae aspernatur, distinctio beatae rerum magni modi a in assumenda libero. Porro!'
+  },
+   {
+    name: "obiora igboanusi",
+    tittle: "front-end developer",
+    role: "Project Manager",
+    image: obiora,
+    socials: {
+      twitter: "https://www.twitter.com/igboanusiobiora",
+      linkedin: "https://www.linkedin.com/c-obioraigboanusi",
+      github: "https://www.github.com/obioraigboanusi",
+    },
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet architecto nulla reprehenderit. Temporibus, odit aliquid? Unde aliquid tempore molestiae aspernatur, distinctio beatae rerum magni modi a in assumenda libero. Porro!'
+  },
+   {
+    name: "obiora igboanusi",
+    tittle: "front-end developer",
+    role: "Project Manager",
+    image: obiora,
+    socials: {
+      twitter: "https://www.twitter.com/igboanusiobiora",
+      linkedin: "https://www.linkedin.com/c-obioraigboanusi",
+      github: "https://www.github.com/obioraigboanusi",
+    },
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet architecto nulla reprehenderit. Temporibus, odit aliquid? Unde aliquid tempore molestiae aspernatur, distinctio beatae rerum magni modi a in assumenda libero. Porro!'
+  },
+   {
+    name: "obiora igboanusi",
+    tittle: "front-end developer",
+    role: "Project Manager",
+    image: obiora,
+    socials: {
+      twitter: "https://www.twitter.com/igboanusiobiora",
+      linkedin: "https://www.linkedin.com/c-obioraigboanusi",
+      github: "https://www.github.com/obioraigboanusi",
+    },
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet architecto nulla reprehenderit. Temporibus, odit aliquid? Unde aliquid tempore molestiae aspernatur, distinctio beatae rerum magni modi a in assumenda libero. Porro!'
+  },
+   {
+    name: "obiora igboanusi",
+    tittle: "front-end developer",
+    role: "Project Manager",
+    image: obiora,
+    socials: {
+      twitter: "https://www.twitter.com/igboanusiobiora",
+      linkedin: "https://www.linkedin.com/c-obioraigboanusi",
+      github: "https://www.github.com/obioraigboanusi",
+    },
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet architecto nulla reprehenderit. Temporibus, odit aliquid? Unde aliquid tempore molestiae aspernatur, distinctio beatae rerum magni modi a in assumenda libero. Porro!'
+  },
+   {
+    name: "obiora igboanusi",
+    tittle: "front-end developer",
+    role: "Project Manager",
+    image: obiora,
+    socials: {
+      twitter: "https://www.twitter.com/igboanusiobiora",
+      linkedin: "https://www.linkedin.com/c-obioraigboanusi",
+      github: "https://www.github.com/obioraigboanusi",
+    },
+    description: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Amet architecto nulla reprehenderit. Temporibus, odit aliquid? Unde aliquid tempore molestiae aspernatur, distinctio beatae rerum magni modi a in assumenda libero. Porro!'
+  },
+ 
+];
